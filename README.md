733ecf9af35873553a42ca6ffea6d93fb2c3fff2
vruivo <vruivo.code@gmail.com>
2023-03-05 23:41:09 +0000

    working code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Read input from a USB keyboard connected to the Pico Micro-USB port, via an OTG cable
+Read input from a USB keyboard connected to the Raspberry Pi Pico via an extra USB port, using Pico_PIO_USB
+
+Based on code from https://github.com/sekigon-gonnoc/Pico-PIO-USB/tree/main/examples/host_hid_to_device_cdc
--- main.c
+++ main.c
@@ -2,40 +2,70 @@
 #include <stdio.h>
 #include <string.h>
 
-// #include "hardware/gpio.h"
-#include "bsp/board.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/bootrom.h"
+
+#include "pio_usb.h"
 #include "tusb.h"
 
-uint8_t kbd_addr = 0;
-uint8_t kbd_inst = 0;
+#include "bsp/board.h"
+#include "hardware/uart.h"
 
-void main() {
+#define UART_ID uart0
+#define BAUD_RATE 9600
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+
+static uint8_t const keycode2ascii[128][2] =  { HID_KEYCODE_TO_ASCII };
+
+static void process_kbd_report(hid_keyboard_report_t const *report);
+
+
+// core1: handle host events
+void core1_main() {
+  sleep_ms(10);
+
+  // Use tuh_configure() to pass pio configuration to the host stack
+  // Note: tuh_configure() must be called before tuh_init()
+  // tuh_configure(rhport, 1 = Pico_PIO, 0 = rp2040 hw/native; config type; config)
+  pio_usb_configuration_t pio_cfg = PIO_USB_DEFAULT_CONFIG;
+  pio_cfg.pin_dp = 4;  // DM = DP+1  (DP = D+ / DM = D-)
+  tuh_configure(1, TUH_CFGID_RPI_PIO_USB_CONFIGURATION, &pio_cfg);
+
+  // To run USB SOF interrupt in core1, init host stack for
+  //   pio_usb (roothub port1) on core1
+  tuh_init(1);
+
+  while (true) {
+    tuh_task(); // tinyusb host task
+  }
+}
+
+// core0: handle device events
+int main(void) {
   board_init();
-  
-  // sm = pio_claim_unused_sm(pio, true);
-  // offset = pio_add_program(pio, &ps2dev_program);
-  // ps2dev_program_init(pio, sm, offset, CLKIN, CLKOUT, DATIN, DATOUT);
-  
-  // gpio_set_irq_enabled_with_callback(CLKIN, GPIO_IRQ_EDGE_RISE, true, &irq_callback);
-  tusb_init();  // TinyUSB init
-  // tuh_init(BOARD_TUH_RHPORT);  // alternative, init host stack on configured roothub port
-  
-  while(true) {
-    tuh_task();
-    
-    // if(!pio_sm_is_rx_fifo_empty(pio, sm)) {
-    //   ps2_receive(pio_sm_get(pio, sm));
-    //   board_led_write(0);
-    // }
-    
-    // if(repeating) {
-    //   repeating = false;
-      
-    //   if(repeat) {
-    //     maybe_send_e0(repeat);
-    //     ps2_send(hid2ps2[repeat]);
-    //   }
-    // }
+  // default 125MHz is not appropreate. Sysclock should be multiple of 12MHz.
+  set_sys_clock_khz(120000, true);
+
+  // init serial
+  uart_init(UART_ID, BAUD_RATE);
+  gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+  sleep_ms(10);
+
+  multicore_reset_core1();
+  // all USB task run in core1
+  multicore_launch_core1(core1_main);
+
+  // turn on onboard led
+  board_led_write(true);
+
+  while (true) {
+    stdio_flush();
+    sleep_us(10);
   }
 
   return 0;
@@ -46,12 +76,14 @@ void main() {
 // TinyUSB Callbacks
 //--------------------------------------------------------------------+
 
+// called after all tuh_hid_mount_cb
 void tuh_mount_cb(uint8_t dev_addr)
 {
   // application set-up
   printf("A device with address %d is mounted\r\n", dev_addr);
 }
 
+// called before all tuh_hid_unmount_cb
 void tuh_umount_cb(uint8_t dev_addr)
 {
   // application tear-down
@@ -66,57 +98,42 @@ void tuh_umount_cb(uint8_t dev_addr)
 // therefore report_desc = NULL, desc_len = 0
 void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len) {
   printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
+
+  (void)desc_report;
+  (void)desc_len;
+
   if(tuh_hid_interface_protocol(dev_addr, instance) == HID_ITF_PROTOCOL_KEYBOARD) {
-    kbd_addr = dev_addr;
-    kbd_inst = instance;
-    
-    // blinking = true;
-    // add_alarm_in_ms(1, blink_callback, NULL, false);
-    
-    tuh_hid_receive_report(dev_addr, instance);
-    // if ( !tuh_hid_receive_report(dev_addr, instance) )
-    // {
-    //   printf("Error: cannot request to receive report\r\n");
-    // }
+
+    if ( !tuh_hid_receive_report(dev_addr, instance) )
+    {
+      printf("Error: cannot request to receive report\r\n");
+    }
   }
 }
 
 // Invoked when device with hid interface is un-mounted
 void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance) {
   printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-  if(dev_addr == kbd_addr && instance == kbd_inst) {
-    kbd_addr = 0;
-    kbd_inst = 0;
-  }
 }
 
 
 
 // Invoked when received report from device via interrupt endpoint
+// called on key press and key release
 void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
 {
-  if( !(dev_addr == kbd_addr && instance == kbd_inst) ) {
-    printf("Invalid dev_addr or instance\r\n");
-    return;
-  }
-
-  // if(report[1] != 0) {    // ???
-  //   tuh_hid_receive_report(dev_addr, instance);
-  //   return;
-  // }
+  printf("received report from HID device address = %d, instance = %d\r\n", dev_addr, instance);
+  (void) len;
 
   uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
 
   switch (itf_protocol)
   {
     case HID_ITF_PROTOCOL_KEYBOARD:
-      TU_LOG2("HID receive boot keyboard report\r\n");
       printf("HID receive boot keyboard report\r\n");
       process_kbd_report( (hid_keyboard_report_t const*) report );
     break;
   }
-
-  // board_led_write(1);  // ?????
 
   // continue to request to receive report
   if ( !tuh_hid_receive_report(dev_addr, instance) )
@@ -159,6 +176,7 @@ static void process_kbd_report(hid_keyboard_report_t const *report)
         bool const is_shift = report->modifier & (KEYBOARD_MODIFIER_LEFTSHIFT | KEYBOARD_MODIFIER_RIGHTSHIFT);
         uint8_t ch = keycode2ascii[report->keycode[i]][is_shift ? 1 : 0];
         putchar(ch);
+        putchar('\n');
         if ( ch == '\r' ) putchar('\n'); // added new line for enter key
 
         fflush(stdout); // flush right away, else nanolib will wait for newline
